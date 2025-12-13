cfdf9d1f9fd44c6815c2bed32450fb282641a04c
eotter-beep <krw122081@gmail.com>
2025-12-13 14:45:50 -0500

    add loading screen

---
+++ nuxt.config.js
@@ -0,0 +1,12 @@
+// Minimal stub config (Nuxt packages removed)
+export default {
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
+  nuxtLoadingIndicator: {
+    color: '#3b82f6',
+    height: '3px',
+    duration: 3000,
+    throttle: 0
+  }
+};
